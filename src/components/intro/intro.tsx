5e219781c0001129b6e61cc5256625bf905ff94f
Daniel Chong <danchong625@gmail.com>
2018-05-22 14:34:49 -0700

    [feat] initializes intro component

---
+++ src/components/intro/intro.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+export class Intro extends React.Component<{}> {
+  render() {
+    return (
+      <div id="intro" uk-height-viewport={"true"}>
+        <div>
+          <h2>Welcome!</h2>
+        </div>
+      </div>
+    )
+  }
+};
