3faf13e5459e5bae9b9dcb52041e3002deb9944f
Daniel Chong <danchong625@gmail.com>
2018-05-22 20:36:23 -0700

    [feat] adds navbar with scroll event w/o collapse

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,44 +1,87 @@
 import * as React from 'react';
 
-export class Navbar extends React.Component<{}> {
+import './navbar.scss';
+
+interface navbarState {
+  backgroundColor: string;
+  leftColor: string;
+  rightColor: string;
+}
+
+export class Navbar extends React.Component<{}, navbarState> {
+  constructor(props: {}) {
+    super(props);
+
+    this.state = {
+      backgroundColor: 'transparent',
+      leftColor: 'white',
+      rightColor: 'white'
+    }
+
+    this.navScrollEvent = this.navScrollEvent.bind(this);
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.navScrollEvent);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.navScrollEvent);
+  }
+
+  private navScrollEvent(e: any) {
+    if (e.target.documentElement.offsetWidth > 768) {
+      const nav = document.querySelector('#navbar');
+      if (e.target.documentElement.scrollTop > 20) {
+        this.setState({
+          backgroundColor: 'white',
+          leftColor: '#0695F5',
+          rightColor: 'black'
+        });
+      } else {
+        this.setState({
+          backgroundColor: 'transparent',
+          leftColor: 'white',
+          rightColor: 'white'
+        });
+      }
+    }
+  }
+
   private smoothScrollTo(key: string) {
     document.querySelector(`#${key}`).scrollIntoView({ behavior: 'smooth' });
   }
 
   render() {
+    const { backgroundColor, leftColor, rightColor } = this.state;
+
     return (
-      <nav className="navbar fixed-top navbar-expand-lg navbar-light container">
-        <div className="navbar-brand">
-          <span
-            onClick={(e) => {
-              e.preventDefault();
-              this.smoothScrollTo('intro');
-            }}
-          >Daniel M Chong</span>
+      <div id="navbar" style={{ backgroundColor }} className="container">
+        <div className="left-nav">
+          <a style={{ color: leftColor }} href="" onClick={(e) => {
+            e.preventDefault();
+            this.smoothScrollTo('intro');
+          }}>Daniel Chong</a>
         </div>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarMenu" aria-controls="navbarMenu" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarMenu">
-          <ul className="navbar-nav nav justify-content-end">
-            <li className="nav-item">
-              <a href="" className="nav-link">Projects</a>
-            </li>
-            <li className="nav-item">
-              <a href="" className="nav-link">Skills</a>
-            </li>
-            <li className="nav-item">
-              <a href="" className="nav-link">About Me</a>
-            </li>
-            <li className="nav-item">
-              <a href="" className="nav-link">Contact Me</a>
-            </li>
-            <li className="nav-item">
-              <a href="https://drive.google.com/file/d/1oBvWXSx6PH0lc80_dNPuAePyurOOgA0T/view" target="_blank" className="nav-link">Resume</a>
-            </li>
-          </ul>
+        <div className="right-nav">
+          <a style={{ color: rightColor }} href="" onClick={(e) => {
+            e.preventDefault();
+            this.smoothScrollTo('proj');
+          }}>Projects</a>
+          <a style={{ color: rightColor }} href="" onClick={(e) => {
+            e.preventDefault();
+            this.smoothScrollTo('tech');
+          }}>Skills</a>
+          <a style={{ color: rightColor }} href="" onClick={(e) => {
+            e.preventDefault();
+            this.smoothScrollTo('bio');
+          }}>About Me</a>
+          <a style={{ color: rightColor }} href="">Contact Me</a>
+          <a style={{ color: rightColor }} href="https://drive.google.com/file/d/1oBvWXSx6PH0lc80_dNPuAePyurOOgA0T/view" target="_blank">
+            Resume
+          </a>
         </div>
-      </nav>
+      </div>
     )
   }
 };
