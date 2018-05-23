26e022beb63e2077697ecc23c2380f5b3b773918
Daniel Chong <danchong625@gmail.com>
2018-05-22 21:03:23 -0700

    [feat] adds name and bio info

--- src/components/intro/intro.tsx
+++ src/components/intro/intro.tsx
@@ -1,11 +1,29 @@
 import * as React from 'react';
 
-export class Intro extends React.Component<{}> {
+import './intro.scss';
+
+interface IntroState {
+  bio: string;
+}
+
+export class Intro extends React.Component<{}, IntroState> {
+  constructor(props: {}) {
+    super(props);
+
+    this.state = {
+      bio: 'I am a software engineer with a passion for collaborating with awesome people to contribute towards building exciting projects that improves everyday life. I enjoy challenging myself with new technologies.',
+    };
+  }
+
   render() {
+    const { bio } = this.state;
+
     return (
-      <div id="intro" uk-height-viewport={"true"}>
-        <div>
-          <h2>Welcome!</h2>
+      <div id="intro" className="component-view">
+        <div className="container">
+          <h1>Daniel M Chong</h1>
+          <hr/>
+          <p>{ bio }</p>
         </div>
       </div>
     )
