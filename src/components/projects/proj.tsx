419b3f9bcb85463e8ec33fc46b7acee6713b12a9
Daniel Chong <danchong625@gmail.com>
2018-05-24 19:19:07 -0700

    [feat] adds scroll method to animate on view

--- src/components/projects/proj.tsx
+++ src/components/projects/proj.tsx
@@ -3,11 +3,54 @@ import * as React from 'react';
 import './proj.scss';
 
 export class Project extends React.Component<{}> {
+  constructor(props: {}) {
+    super(props);
+
+    this.handleScroll = this.handleScroll.bind(this);
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.handleScroll);
+  }
+
+  private handleAnimation(el: HTMLDivElement, top: number) {
+    const hasClass = el.classList.contains('animated');
+
+    if (!hasClass && top > el.offsetTop) {
+      el.classList.add('bounceInRight');
+      el.classList.add('animated');
+    }
+  }
+
+  private handleScroll(e: any) {
+    const projects = document.querySelectorAll('.project-item');
+
+    const scrollFromTop = window.pageYOffset;
+    const windowHeight = window.innerHeight;
+
+    projects.forEach((el, i) => {
+      this.handleAnimation(el as HTMLDivElement, windowHeight + scrollFromTop);
+    });
+  }
+
   render() {
     return (
-      <div id="proj" className="component-view">
-        <div className="container">
-          hi
+      <div>
+        <div id="proj" className="component-view">
+          <div className="container">
+            <div className="component-title">
+              <h2>PROJECTS</h2>
+            </div>
+            <div className="proj-body">
+              <div className="project-item">
+                hello
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     )
