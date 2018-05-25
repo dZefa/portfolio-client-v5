9e068d264490dc8a962ba7697eb5045f536b7d4a
Daniel Chong <danchong625@gmail.com>
2018-05-24 17:30:23 -0700

    [feat] moves container out to own div

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -56,30 +56,32 @@ export class Navbar extends React.Component<{}, navbarState> {
     const { backgroundColor, leftColor, rightColor } = this.state;
 
     return (
-      <div id="navbar" style={{ backgroundColor }} className="container">
-        <div className="left-nav">
-          <a style={{ color: leftColor }} href="" onClick={(e) => {
-            e.preventDefault();
-            this.smoothScrollTo('intro');
-          }}>Daniel Chong</a>
-        </div>
-        <div className="right-nav">
-          <a style={{ color: rightColor }} href="" onClick={(e) => {
-            e.preventDefault();
-            this.smoothScrollTo('proj');
-          }}>Projects</a>
-          <a style={{ color: rightColor }} href="" onClick={(e) => {
-            e.preventDefault();
-            this.smoothScrollTo('tech');
-          }}>Skills</a>
-          <a style={{ color: rightColor }} href="" onClick={(e) => {
-            e.preventDefault();
-            this.smoothScrollTo('bio');
-          }}>About Me</a>
-          <a style={{ color: rightColor }} href="">Contact Me</a>
-          <a style={{ color: rightColor }} href="https://drive.google.com/file/d/1oBvWXSx6PH0lc80_dNPuAePyurOOgA0T/view" target="_blank">
-            Resume
-          </a>
+      <div id="navbar" style={{ backgroundColor }}>
+        <div className="container">
+          <div className="left-nav">
+            <a style={{ color: leftColor }} href="" onClick={(e) => {
+              e.preventDefault();
+              this.smoothScrollTo('intro');
+            }}>Daniel Chong</a>
+          </div>
+          <div className="right-nav">
+            <a style={{ color: rightColor }} href="" onClick={(e) => {
+              e.preventDefault();
+              this.smoothScrollTo('proj');
+            }}>Projects</a>
+            <a style={{ color: rightColor }} href="" onClick={(e) => {
+              e.preventDefault();
+              this.smoothScrollTo('tech');
+            }}>Skills</a>
+            <a style={{ color: rightColor }} href="" onClick={(e) => {
+              e.preventDefault();
+              this.smoothScrollTo('bio');
+            }}>About Me</a>
+            <a style={{ color: rightColor }} href="">Contact Me</a>
+            <a style={{ color: rightColor }} href="https://drive.google.com/file/d/1oBvWXSx6PH0lc80_dNPuAePyurOOgA0T/view" target="_blank">
+              Resume
+            </a>
+          </div>
         </div>
       </div>
     )
