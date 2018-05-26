ebeeab10923659c33a16288be337cb08692cec9a
Daniel Chong <danchong625@gmail.com>
2018-05-25 21:08:58 -0700

    [feat] fixes key in mapping of skill list

--- src/components/skills/skillList.tsx
+++ src/components/skills/skillList.tsx
@@ -12,14 +12,14 @@ export class SkillList extends React.Component<SkillListProps> {
 
   render() {
     const { title, list } = this.props;
-    
+
     return (
       <div className="skill-list">
         <h4>{ title }</h4>
         <ul>
           {
             list.map((item, i) => {
-              return <li>{ item }</li>
+              return <li key={`${title}-list-${i}`}>{ item }</li>
             })
           }
         </ul>
