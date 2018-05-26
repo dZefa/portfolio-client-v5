6d93d5d6d993b85c835b478c2c8af22690eae598
Daniel Chong <danchong625@gmail.com>
2018-05-26 16:45:30 -0700

    [feat] adds props and state interface to contactme component

--- src/components/contact/contactMe.tsx
+++ src/components/contact/contactMe.tsx
@@ -2,10 +2,37 @@ import * as React from 'react';
 
 import './contactMe.scss';
 
-export class ContactMe extends React.Component<{}> {
+interface ContactMeProps {
+  hidden: boolean;
+  toggleModal: VoidFunction;
+};
+
+interface ContactMeState {
+  firstName: string;
+  lastName: string;
+  email: string;
+  subject: string;
+  message: string;
+};
+
+export class ContactMe extends React.Component<ContactMeProps, ContactMeState> {
+  constructor(props: ContactMeProps) {
+    super(props);
+
+    this.state = {
+      firstName: '',
+      lastName: '',
+      email: '',
+      subject: '',
+      message: '',
+    };
+  }
+
   render() {
+    const { hidden, toggleModal } = this.props;
+    
     return (
-      <div>
+      <div hidden={hidden}>
         contact me!
       </div>
     )
