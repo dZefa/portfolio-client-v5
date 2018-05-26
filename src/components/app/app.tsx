04ffb854e1ac53af34a2320791fae2b91b346020
Daniel Chong <danchong625@gmail.com>
2018-05-26 16:43:08 -0700

    [feat] adds modal state and toggler to app component

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -6,11 +6,34 @@ import { Project } from '../projects/proj';
 import { Skills } from '../skills/skills';
 import { Bio } from '../bio/bio';
 import { Footer } from '../footer/footer';
+import { ContactMe } from '../contact/contactMe';
 
-export class App extends React.Component<{}> {
+interface AppState {
+  modalHidden: boolean;
+};
+
+export class App extends React.Component<{}, AppState> {
+  constructor(props: {}) {
+    super(props);
+
+    this.state = {
+      modalHidden: true,
+    };
+
+    this.toggleModal = this.toggleModal.bind(this);
+  }
+
+  toggleModal() {
+    this.setState({
+      modalHidden: !this.state.modalHidden,
+    });
+  }
+
   render() {
+    const { modalHidden } = this.state;
     return (
       <div>
+        <ContactMe hidden={modalHidden} toggleModal={this.toggleModal} />
         <Navbar />
         <Intro />
         <Project />
