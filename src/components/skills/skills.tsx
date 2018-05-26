f7a5b28a64b52424c77fb76939ae17b415d5cfe4
Daniel Chong <danchong625@gmail.com>
2018-05-25 21:09:56 -0700

    [feat] adds rest of skill lists and scroll handler for animation

--- src/components/skills/skills.tsx
+++ src/components/skills/skills.tsx
@@ -13,22 +13,74 @@ interface SkillState {
     title: string;
     list: Array<string>;
   }
-}
+};
 
 export class Skills extends React.Component<{}, SkillState> {
   constructor(props: {}) {
-    super({});
+    super(props);
 
     this.state = {
       strongList: {
         title: 'STRONG',
-        list: ['JavaScript/TypeScript'],
+        list: [
+          'JavaScript/TypeScript',
+          'HTML5/CSS3/SASS/SCSS',
+          'jQuery/AJAX/JSON',
+          'React.js/Redux/Webpack/Babel',
+          'Node.js/Express/JWT',
+          'PostgreSQL, NoSQL',
+          'AWS(S3, EC2, RDS, Cognito, etc)',
+          'Jest/Enzyme/Mocha/Chai',
+          'Electron',
+          'Git'
+        ],
       },
       expList: {
         title: 'EXPERIENCED',
-        list: ['C#'],
+        list: [
+          'C#',
+          'Python/Flask',
+          'Java',
+          'Unity',
+          'Android Studio',
+          'Firebase/AdMob/Google Play',
+          'Socket.io',
+          'Angular.js/Backbone.js',
+          'MongoDB/MySQL'
+        ],
       },
     };
+
+    this.handleScroll = this.handleScroll.bind(this);
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll);
+  }
+
+  componentWillMount() {
+    window.removeEventListener('scroll', this.handleScroll);
+  }
+
+  private handleAnimation(el: HTMLDivElement, top: number) {
+    const hasClass = el.classList.contains('animated');
+
+    if (!hasClass && top > el.offsetTop) {
+      el.classList.add('showProject');
+      el.classList.add('fadeIn');
+      el.classList.add('animated');
+    }
+  }
+
+  private handleScroll(e: any) {
+    const skills = document.querySelectorAll('.skill-list');
+
+    const scrollFromTop = window.pageYOffset;
+    const windowHeight = window.innerHeight;
+
+    skills.forEach((el, i) => {
+      this.handleAnimation(el as HTMLDivElement, windowHeight + scrollFromTop);
+    });
   }
 
   render() {
