12e3a6a013b9de8d3592bdd732e1300449829d01
Daniel Chong <danchong625@gmail.com>
2018-05-26 01:59:18 -0700

    [feat] initializes footer component and styling

---
+++ src/components/footer/footer.tsx
@@ -0,0 +1,36 @@
+import * as React from 'react';
+
+import './footer.scss';
+
+interface FooterState {
+  name: string;
+  phone: string;
+  email: string;
+};
+
+export class Footer extends React.Component<{}, FooterState> {
+  constructor(props: {}) {
+    super(props);
+
+    this.state = {
+      name: 'Daniel M Chong',
+      phone: '213-550-8565',
+      email: 'danchong625@gmail.com'
+    }
+  }
+  render() {
+    const { name, phone, email } = this.state;
+
+    return (
+      <div>
+        <div id="footer">
+          <div className="container">
+            <p>{ name }</p>
+            <p>{ phone }</p>
+            <a href={`mailto:${ email }`} target="_blank">{ email }</a>
+          </div>
+        </div>
+      </div>
+    )
+  }
+};
