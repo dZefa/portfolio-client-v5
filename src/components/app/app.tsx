99168e8b2c99dc5301f57efaa77fcf7c9b691ff8
Daniel Chong <danchong625@gmail.com>
2018-05-26 16:59:06 -0700

    [feat] passes down hidden and toggle from app to nav

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -34,7 +34,7 @@ export class App extends React.Component<{}, AppState> {
     return (
       <div>
         <ContactMe hidden={modalHidden} toggleModal={this.toggleModal} />
-        <Navbar toggleModal={this.toggleModal} />
+        <Navbar hidden={modalHidden} toggleModal={this.toggleModal} />
         <Intro />
         <Project />
         <Skills />
