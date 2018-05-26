3ed8d900e1a8c96b7db7964b626b4d8a98009bfb
Daniel Chong <danchong625@gmail.com>
2018-05-26 16:35:21 -0700

    [feat] initializes contactMe component and styling

---
+++ src/components/contact/contactMe.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+import './contactMe.scss';
+
+export class ContactMe extends React.Component<{}> {
+  render() {
+    return (
+      <div>
+        contact me!
+      </div>
+    )
+  }
+};
