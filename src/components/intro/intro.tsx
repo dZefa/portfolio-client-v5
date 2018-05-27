92d21511b29c3da626d98771dfbdf522b997a2f4
Daniel Chong <danchong625@gmail.com>
2018-05-26 22:27:23 -0700

    [feat] adds buttons to link to projects on intro

--- src/components/intro/intro.tsx
+++ src/components/intro/intro.tsx
@@ -11,12 +11,14 @@ export class Intro extends React.Component<{}, IntroState> {
     super(props);
 
     this.state = {
-      bio: 'I am a software engineer with a passion for collaborating with awesome people to contribute towards building exciting projects that improves everyday life. I enjoy challenging myself with new technologies.',
+      bio: 'I am a software engineer with a passion for collaborating with awesome people to contribute towards building exciting projects that improves everyday life. I enjoy learning and using new technologies to come up with unique solutions.',
     };
   }
 
   render() {
     const { bio } = this.state;
+
+    console.log(`Hi There! Visit https://www.danielmchong.com/rito for a more personal and vanilla JS website!`);
 
     return (
       <div id="intro" className="component-view">
@@ -24,6 +26,10 @@ export class Intro extends React.Component<{}, IntroState> {
           <h1>Daniel M Chong</h1>
           <hr/>
           <p>{ bio }</p>
+          <div className="intro-buttons">
+            <a href="https://github.com/dzefa" target="_blank">Take a Look at My Projects</a>
+            <a href="https://github.com/dZefa/portfolio-client-v5" target="_blank">Code Behind My Website</a>
+          </div>
         </div>
       </div>
     )
